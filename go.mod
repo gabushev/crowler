ec1680d5bb3cff808878dd5e1299c24f50e7db1d
Ilia Gabushev <ilya.gabushev@fahecom.com>
2024-02-20 17:53:41 +0100

    add basics and cfg

---
+++ go.mod
@@ -0,0 +1,9 @@
+module crawler
+
+go 1.21
+
+require (
+	golang.org/x/net v0.21.0
+	golang.org/x/sync v0.6.0
+	gopkg.in/yaml.v3 v3.0.1
+)
---
+++ internal/cfg/config.go
@@ -0,0 +1,24 @@
+package cfg
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Config struct {
+	Parallelism         int      `yaml:"parallelism"`
+	AcceptableMimeTypes []string `yaml:"acceptable_mime_types"`
+}
+
+func NewConfig(configPath string) (*Config, error) {
+	buf, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(buf, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
